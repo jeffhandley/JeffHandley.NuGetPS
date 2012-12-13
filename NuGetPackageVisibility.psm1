02faf8d5c7b3f105d23454958b9a7344be3c9abd
Jeff Handley <jeff@jeffhandley.com>
2012-12-13 09:44:37 -0800

    Moving Submit-Package to the top of the file.

--- NuGetPackageVisibility.psm1
+++ NuGetPackageVisibility.psm1
@@ -1,3 +1,70 @@
+function Submit-Package {
+  <#
+  .SYNOPSIS
+  Submits a NuGet package (or set of packages) to the gallery, but as hidden (unlisted).
+  .DESCRIPTION
+  Uploads the specified package (or all packages from a packages.config file) to the gallery and then immediately marks the package(s) as unlisted by running the nuget delete command.
+  .EXAMPLE
+  Submit-Package -packageId MyAwesomePackage -packageVersion 2.0.0.0 -packageFile MyAwesomePackage.2.0.0.nupkg -apiKey 00000000-0000-0000-0000-000000000000 -galleryUrl https://nuget.org
+  .EXAMPLE
+  Submit-Package -packagesConfig packages.config -apiKey 00000000-0000-0000-0000-000000000000 -galleryUrl https://nuget.org
+  .PARAMETER packageId
+  The Id of the package to hide/show
+  .PARAMETER packageVersion
+  The Version of the package to hide/show
+  .PARAMETER packageFile
+  The nupkg file to upload for the NuGet package
+  .PARAMETER packagesConfig
+  The XML config file that lists the packages to be hidden/shown
+  .PARAMETER galleryUrl
+  The NuGet gallery Url to connect to.  By default, https://nuget.org
+  #>
+  param(
+    [Parameter(ParameterSetName="package")] $packageId,
+    [Parameter(ParameterSetName="package")] $packageVersion,
+    [Parameter(ParameterSetName="package")] $packageFile,
+    [Parameter(ParameterSetName="config")]  $packagesConfig,
+    $apiKey,
+    $galleryUrl = "https://nuget.org"
+  )
+
+  If ($apiKey -eq $null) { throw "Parameter 'apiKey' was not specified" }
+  If ($galleryUrl -eq $null) { throw "Parameter 'galleryUrl' was not specified" }
+
+  If ($PSCmdlet.ParameterSetName -match "package") {
+    If ($packageId -eq $null) { throw "Parameter 'packageId' was not specified" }
+    If ($packageVersion -eq $null) { throw "Parameter 'packageVersion' was not specified" }
+    If ($packageFile -eq $null) { throw "Parameter 'packageFile' was not specified" }
+
+    $exists = Test-Path $packageFile
+    if ($exists -eq $false)
+    {
+      throw "File not found: $packageFile"
+    }
+
+    PushDelete -packageId $packageId -packageVersion $packageVersion -packageFile $packageFile -apiKey $apiKey -galleryUrl $galleryUrl
+  }
+  ElseIf ($PSCmdlet.ParameterSetName -match "config") {
+    If ($packagesConfig -eq $null) { throw "Parameter 'packagesConfig' was not specified" }
+    If (!(Test-Path $packagesConfig)) { throw "File '$packagesConfig' was not found" }
+
+    [xml]$packages = Get-Content $packagesConfig
+
+    foreach ($package in $packages.packages.package) {
+      $path = ".\" + $package.culture + "\" + $package.id + "." + $package.version + ".nupkg"
+      $path = $path.Replace("\\", "\")
+
+      $exists = Test-Path $path
+      if ($exists -eq $false)
+      {
+        throw "File not found: $path"
+      }
+
+      PushDelete -packageId $package.id -packageVersion $package.version -packageFile $path -apiKey $apiKey -galleryUrl $galleryUrl
+    }
+  }
+}
+
 function Set-NuGetPackageVisibility {
   <#
   .SYNOPSIS
@@ -117,73 +184,6 @@ function Show-NuGetPackage {
   }
   Else {
     Set-NuGetPackageVisibility -action show -packageId $packageId -packageVersion $packageVersion -apiKey $apiKey -galleryUrl $galleryUrl
-  }
-}
-
-function Submit-Package {
-  <#
-  .SYNOPSIS
-  Submits a NuGet package (or set of packages) to the gallery, but as hidden (unlisted).
-  .DESCRIPTION
-  Uploads the specified package (or all packages from a packages.config file) to the gallery and then immediately marks the package(s) as unlisted by running the nuget delete command.
-  .EXAMPLE
-  Submit-Package -packageId MyAwesomePackage -packageVersion 2.0.0.0 -packageFile MyAwesomePackage.2.0.0.nupkg -apiKey 00000000-0000-0000-0000-000000000000 -galleryUrl https://nuget.org
-  .EXAMPLE
-  Submit-Package -packagesConfig packages.config -apiKey 00000000-0000-0000-0000-000000000000 -galleryUrl https://nuget.org
-  .PARAMETER packageId
-  The Id of the package to hide/show
-  .PARAMETER packageVersion
-  The Version of the package to hide/show
-  .PARAMETER packageFile
-  The nupkg file to upload for the NuGet package
-  .PARAMETER packagesConfig
-  The XML config file that lists the packages to be hidden/shown
-  .PARAMETER galleryUrl
-  The NuGet gallery Url to connect to.  By default, https://nuget.org
-  #>
-  param(
-    [Parameter(ParameterSetName="package")] $packageId,
-    [Parameter(ParameterSetName="package")] $packageVersion,
-    [Parameter(ParameterSetName="package")] $packageFile,
-    [Parameter(ParameterSetName="config")]  $packagesConfig,
-    $apiKey,
-    $galleryUrl = "https://nuget.org"
-  )
-
-  If ($apiKey -eq $null) { throw "Parameter 'apiKey' was not specified" }
-  If ($galleryUrl -eq $null) { throw "Parameter 'galleryUrl' was not specified" }
-
-  If ($PSCmdlet.ParameterSetName -match "package") {
-    If ($packageId -eq $null) { throw "Parameter 'packageId' was not specified" }
-    If ($packageVersion -eq $null) { throw "Parameter 'packageVersion' was not specified" }
-    If ($packageFile -eq $null) { throw "Parameter 'packageFile' was not specified" }
-
-    $exists = Test-Path $packageFile
-    if ($exists -eq $false)
-    {
-      throw "File not found: $packageFile"
-    }
-
-    PushDelete -packageId $packageId -packageVersion $packageVersion -packageFile $packageFile -apiKey $apiKey -galleryUrl $galleryUrl
-  }
-  ElseIf ($PSCmdlet.ParameterSetName -match "config") {
-    If ($packagesConfig -eq $null) { throw "Parameter 'packagesConfig' was not specified" }
-    If (!(Test-Path $packagesConfig)) { throw "File '$packagesConfig' was not found" }
-
-    [xml]$packages = Get-Content $packagesConfig
-
-    foreach ($package in $packages.packages.package) {
-      $path = ".\" + $package.culture + "\" + $package.id + "." + $package.version + ".nupkg"
-      $path = $path.Replace("\\", "\")
-
-      $exists = Test-Path $path
-      if ($exists -eq $false)
-      {
-        throw "File not found: $path"
-      }
-
-      PushDelete -packageId $package.id -packageVersion $package.version -packageFile $path -apiKey $apiKey -galleryUrl $galleryUrl
-    }
   }
 }
 
